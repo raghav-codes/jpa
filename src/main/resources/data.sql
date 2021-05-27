f27d837dd1529f3ee52131da91b599d084456cba
raghav-codes <raghavendra.balaji@gmail.com>
2021-05-26 23:36:42 -0400

    JPA initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+create table person
+(
+
+id integer not null,
+name varchar(255) not null,
+location varchar(255),
+birthday timestamp,
+primary key(id)
+);
+
+insert into person(id,name,location,birthday)
+values(10001,'Raghav','canada',sysdate());
